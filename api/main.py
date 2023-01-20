53158a7cb047cc5f0d7de98ba887bc8fdd15a942
trancenoid <shivansh.bhr@gmail.com>
2023-01-20 22:37:45 +0530

    test and fix API receive file endpoint

--- api/main.py
+++ api/main.py
@@ -1,21 +1,41 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile, Form
 from PIL import Image
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import io
+from SD2Model.impainting import *
+
+
+class Txt2ImgModel(BaseModel):
+    txt : str
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/isalive")
 def isalive():
     return {"message" : True}
 
-@app.post("/impaint")
-async def impaint(image : UploadFile, mask : UploadFile, prompt : str):
+@app.post("/impaint/")
+async def impaint(image : UploadFile, mask : UploadFile , prompt : str = Form()):
+    print(image.filename,mask.filename,prompt)
+
     image_bytes = await image.read()
-    image = Image.open(io.BytesIO(image_bytes))
+    image_ = Image.open(io.BytesIO(image_bytes))
+    image_.save(image.filename)
+
     mask_bytes = await mask.read()
-    mask = Image.open(io.BytesIO(mask_bytes))
+    mask_ = Image.open(io.BytesIO(mask_bytes))
+    mask_.save(mask.filename)
 
     return {"Image/Mask" : f"{image.filename}/{mask.filename}"}
 
-@app.post("/txt2img")
-async def txt2img(txt : str):
-    return {"message": "Yo!"}
+@app.post("/txt2img/")
+async def txt2img(body : Txt2ImgModel):
+    return {"message": f"Yo! you entered {body.txt}"}
